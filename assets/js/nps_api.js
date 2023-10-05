0f3b52b5eac83f8eb4b6748070d1dd320e27cd97
Justin Patty <justinpatty54@gmail.com>
2023-10-04 20:31:32 -0500

    cards move with slected objects

--- assets/js/nps_api.js
+++ assets/js/nps_api.js
@@ -28,16 +28,18 @@ document
     var stateSelect = document.querySelector("#state-dropdown").value;
     getParks(stateSelect);
   });
-//${state}
-function getParks(state) {
+  function getParks(state) {
   const requestUrl = `${baseURL}/parks?stateCode=${state}&api_key=${apiKey}`;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      console.log(data);
+      var f = false;
+      cards(data, f);
       const parks = data.data.filter(function (item) {
-        return item.fullName.toLowerCase().includes("park");
+      return item.fullName.toLowerCase().includes("park");
       });
       showParkDropdown(parks);
     })
@@ -62,7 +64,7 @@ document
     getCampGrounds(park);
   });
 
-function getCampGrounds(park) {
+function getCampGrounds(park)  {
   const requestUrl = `${baseURL}/campgrounds?parkCode=${park}&api_key=${apiKey}`;
   fetch(requestUrl)
     .then(function (response) {
@@ -97,10 +99,12 @@ document
   });
 
 function showCampInfo(camp) {
-    const content = `
-    <img src="${camp.images[0].url}" alt="${camp.images[0].altText}">
-            <h3>Name: ${camp.name}</h3>
-            <p>${camp.description}</p>`
+ //   const content = `
+  var t = true;
+    cards(camp, t);
+   // <img src="${camp.images[0].url}" alt="${camp.images[0].altText}">
+  //        <h3>Name: ${camp.name}</h3>
+  //         <p>${camp.description}</p>`
     document.querySelector(".camp-info").innerHTML=content
 }
 
@@ -138,3 +142,78 @@ function getParkAmenities() {
 }
 
 getStates();
+
+  function cards(data, tf) {
+
+  var main = document.querySelector("main");
+  main.innerHTML = '';
+  
+ if (tf == true) {
+  var div = document.createElement("div");
+  div.setAttribute("class","max-w-sm rounded overflow-hidden shadow-lg bg-green-500 mb-8");
+  main.appendChild(div);
+  
+  var img = document.createElement('img');
+  img.setAttribute("class", "w-full");
+  img.setAttribute("src", "https://media.tenor.com/JgPFGWYaS_8AAAAM/thisistheonedude123-bears.gif");
+  img.setAttribute("alt", "Picture of destination");
+  div.appendChild(img);
+ 
+  var mapdiv = document.createElement("div");
+  mapdiv.setAttribute("id","map-container");
+  div.appendChild(mapdiv);
+
+  var div2 = document.createElement("div");
+     div2.setAttribute("class","px-6 py-4");
+     div.appendChild(div2);
+ 
+     var div2a = document.createElement("div");
+     div2a.setAttribute("class","font-bold text-xl mb-2");
+     var fullName = data.Name;
+     div2a.textContent = fullName;
+     div2.appendChild(div2a);
+ 
+     var p = document.createElement("p");
+     p.setAttribute("class","text-gray-700 text-base");
+     p.textContent = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.";
+     div2.appendChild(p);
+ 
+     var div3 = document.createElement("div");
+     div3.setAttribute("class","px-6 pt-4 pb-2");
+     div.appendChild(div3);
+
+ } else {
+ for (var i = 0; i < data.data.length; i++) {
+     console.log(i);
+     var div = document.createElement("div");
+     div.setAttribute("class","max-w-sm rounded overflow-hidden shadow-lg bg-green-500 mb-8");
+     main.appendChild(div);
+     
+     var img = document.createElement('img');
+     img.setAttribute("class", "w-full");
+     img.setAttribute("src", "https://media.tenor.com/JgPFGWYaS_8AAAAM/thisistheonedude123-bears.gif");
+     img.setAttribute("alt", "Picture of destination");
+     div.appendChild(img);
+    
+     var div2 = document.createElement("div");
+     div2.setAttribute("class","px-6 py-4");
+     div.appendChild(div2);
+ 
+     var div2a = document.createElement("div");
+     div2a.setAttribute("class","font-bold text-xl mb-2");
+     var fullName = data.data[i].fullName
+     div2a.textContent = fullName;
+     div2.appendChild(div2a);
+ 
+     var p = document.createElement("p");
+     p.setAttribute("class","text-gray-700 text-base");
+     p.textContent = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.";
+     div2.appendChild(p);
+ 
+     var div3 = document.createElement("div");
+     div3.setAttribute("class","px-6 pt-4 pb-2");
+     div.appendChild(div3);
+    }
+  }
+} 
+ 
